2231bb477c60bfc6dd6471b1dad535c7b2a9d604
Richard Wagstaff <richard.wagstar@gmail.com>
2017-02-24 09:49:36 +0000

    Add seed project with babel transpiler

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let gameName = 'Snap';
+console.log(`Starting game ${gameName}`);
